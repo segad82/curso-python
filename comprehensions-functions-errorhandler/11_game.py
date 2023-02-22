a12e4fee1ca4145214e978be0144e3121f79bc2e
Kevin Lopez G <kevin.lopez.gonzalez.it@gmail.com>
2023-02-22 11:45:48 -0300

    Se refactoriza juego de 'piedra, papel o tijera' mediante funciones

--- comprehensions-functions-errorhandler/11_game.py
+++ comprehensions-functions-errorhandler/11_game.py
@@ -1,34 +1,23 @@
 import random
 
-options = ('piedra', 'papel', 'tijera')
-
-computer_wins = 0
-user_wins = 0
-
-rounds = 1
-
-while True:
-    print('*' * 10)
-    print('ROUND', rounds)
-    print('*' * 10)
-
-    print('computer_winds', computer_wins)
-    print('user_wins', user_wins)
+def choose_options():
+    options = ('piedra', 'papel', 'tijera')
 
     user_option = input('piedra, papel o tijera =>')
     user_option = user_option.lower()
 
-    rounds += 1
-
     if not user_option in options:
         print('Esa opción no es válida')
-        continue
+        return None, None
 
     computer_option = random.choice(options)
 
     print('User option =>', user_option)
     print('Computer option =>', computer_option)
 
+    return user_option, computer_option
+
+def check_rules(user_option, computer_option, user_wins, computer_wins):
     if user_option == computer_option:
         print('Empate')
     elif user_option == 'piedra':
@@ -59,10 +48,40 @@ while True:
             print('Computer ganó!')
             computer_wins += 1
     
+    return user_wins, computer_wins
+
+def check_winner(user_wins, computer_wins):
+    result = False
     if computer_wins == 2:
         print('El ganador es la computadora')
-        break
+        result = True
 
     if user_wins == 2:
         print('El ganador es el usuario')
-        break
+        result = True
+
+    return result
+
+def run_game():
+    computer_wins = 0
+    user_wins = 0
+    rounds = 1
+
+    while True:
+        print('*' * 10)
+        print('ROUND', rounds)
+        print('*' * 10)
+
+        print('computer_winds', computer_wins)
+        print('user_wins', user_wins)
+
+        rounds += 1
+
+        user_option, computer_option = choose_options()
+
+        user_wins, computer_wins = check_rules(user_option, computer_option, user_wins, computer_wins)
+
+        if check_winner(user_wins, computer_wins) == True:
+            break
+
+run_game()
