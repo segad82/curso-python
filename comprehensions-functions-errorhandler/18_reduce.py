8185c3d2f1604317168e976e028f88c7330614d7
Kevin Lopez G <kevin.lopez.gonzalez.it@gmail.com>
2023-02-23 10:40:54 -0300

    Se termina explicación de la función 'reduce'

--- comprehensions-functions-errorhandler/18_reduce.py
+++ comprehensions-functions-errorhandler/18_reduce.py
@@ -1,5 +1,24 @@
 import functools
 
 numbers = [1,2,3,4]
+
+def accum(counter, item):
+    print('counter: ', counter)
+    print('item: ', item)
+    return counter + item
+'''
+counter:  1
+item:  2 
+
+counter:  3
+item:  3   
+
+counter:  6
+item:  4  
+'''
+
+result = functools.reduce(accum, numbers)
+print(result) # 10
+
 result = functools.reduce(lambda counter, item : counter + item, numbers)
-print(result)
+print(result) # 10
