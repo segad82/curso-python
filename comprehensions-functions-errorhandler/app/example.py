cc8817de762ef4ac6ea31fe14ce10e805fb71e52
Kevin Lopez G <kevin.lopez.gonzalez.it@gmail.com>
2023-02-23 12:29:09 -0300

    Se modifica archivo 'main' para que pueda ser accedido como módulo en nuevo archivo 'example'

---
+++ comprehensions-functions-errorhandler/app/example.py
@@ -0,0 +1,4 @@
+import main
+
+print('example => ', main.data) # example =>  [{'country': 'Colombia', 'population': 400}, {'country': 'Chile', 'population': 300}]
+main.run()
--- comprehensions-functions-errorhandler/app/main.py
+++ comprehensions-functions-errorhandler/app/main.py
@@ -1,16 +1,24 @@
 import utils
 
-keys, values = utils.get_population()
-print(keys, values) # ['col', 'clp'] [400, 300]
-
 data = [
     { 'country': 'Colombia', 'population': 400 },
     { 'country': 'Chile', 'population': 300 }
 ]
 
-result = utils.population_by_country(data, 'Colombia')
-print(result) # [{'country': 'Colombia', 'population': 400}]
+def run():
+    keys, values = utils.get_population()
+    print(keys, values) # ['col', 'clp'] [400, 300]
 
-country = input('Type Country => ')
-result = utils.population_by_country(data, country)
-print(result)
+    result = utils.population_by_country(data, 'Colombia')
+    print(result) # [{'country': 'Colombia', 'population': 400}]
+
+    country = input('Type Country => ')
+    result = utils.population_by_country(data, country)
+    print(result)
+
+'''
+Si este archivo es ejecutado desde la terminal, ejecute esta condición.
+Si este archivo es ejecutado desde otro archivo, no ejecute esta condición.
+'''
+if __name__ == '__main__':
+    run()
