4e09f5d703e4dc308b801548dd3fdb8acd3d6976
Kevin Lopez G <kevin.lopez.gonzalez.it@gmail.com>
2023-02-25 19:32:40 -0300

    Se implementa fastapi y uvicorn en el proyecto web-server

--- pip-virtual-environments/web-server/main.py
+++ pip-virtual-environments/web-server/main.py
@@ -1,4 +1,23 @@
 import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3,4]
+
+@app.get('/contact')
+def get_list():
+    return { 'name': 'Platzi' }
+
+@app.get('/web', response_class=HTMLResponse)
+def get_list():
+    return '''
+    <h1>Ejemplo de pagina web</h1>
+    <p>Esto es una muestra de como renderizar paginas web con FastAPI.</p>
+    '''
 
 def run():
     store.get_categories()
