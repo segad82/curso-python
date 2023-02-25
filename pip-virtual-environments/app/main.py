4abe631c294a258b78046b3a2063b3ff84d9087f
Kevin Lopez G <kevin.lopez.gonzalez.it@gmail.com>
2023-02-25 19:31:31 -0300

    Se implementa la dependencia de pandas

--- pip-virtual-environments/app/main.py
+++ pip-virtual-environments/app/main.py
@@ -1,14 +1,26 @@
+import pandas as pd
 import utils
 import charts
 from read_csv import read_csv
 
 def run():
+
+    '''
     data = read_csv('data.csv')
     data = list(filter(lambda element : element['Continent'] == 'South America', data))
-
     countries = list(map(lambda element : element['Country/Territory'], data))
     values = list(map(lambda element : float(element['World Population Percentage']), data))
+    '''
+
+    dataframe = pd.read_csv('data.csv')
+
+    data = dataframe[dataframe['Continent'] == 'South America']
+    countries = data['Country/Territory'].values
+    values = data['World Population Percentage'].values
+
     charts.generate_pie_chart(countries, values)
+
+    data = read_csv('data.csv')
 
     country = input('Type Country => ')
     result = utils.population_by_country(data, country)
