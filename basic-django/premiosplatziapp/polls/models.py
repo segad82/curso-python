43af8614bfce1936577f5d4747e6b575f51635ab
Kevin Lopez G <kevin.lopez.gonzalez.it@gmail.com>
2023-02-28 17:42:55 -0300

    Se actualiza archivo de notas

--- basic-django/premiosplatziapp/polls/models.py
+++ basic-django/premiosplatziapp/polls/models.py
@@ -30,5 +30,6 @@ class Choice(models.Model):
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
 
+    # Este método indica cómo mostrar la representación string del objeto.
     def __str__(self):
         return self.choice_text
