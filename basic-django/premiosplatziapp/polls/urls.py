cfc8fe15a042b68603884a661c0feb0740e2a7b7
Kevin Lopez G <kevin.lopez.gonzalez.it@gmail.com>
2023-03-01 13:13:09 -0300

    Se agregan cambios relacionados a las vistas django y se actualizan las notas

--- basic-django/premiosplatziapp/polls/urls.py
+++ basic-django/premiosplatziapp/polls/urls.py
@@ -2,5 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    # ex: ~/polls/ => Estás en la página principal de Premios Platzi App
+    path('', views.index, name='index'),
+    # ex: ~/polls/2/ => Estás viendo la pregunta número 2
+    path('<int:question_id>/', views.detail, name='detail'),
+    # ex: ~/polls/3/result => Estás viendo los resultados de la pregunta número 3
+    path('<int:question_id>/result', views.result, name='result'),
+    # ex: ~/polls/4/vote => Estás votando a la pregunta número 4
+    path('<int:question_id>/vote', views.vote, name='vote'),
 ]
--- basic-django/premiosplatziapp/polls/views.py
+++ basic-django/premiosplatziapp/polls/views.py
@@ -2,5 +2,18 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# Las Function Based Views siempre traen como parámetro un HttpRequest
 def index(request):
-    return HttpResponse('Hola Mundo')
+    return HttpResponse('Estás en la página principal de Premios Platzi App')
+
+# En python es buena práctica dejar dos espacios en blanco entre funciones
+def detail(request, question_id):
+    return HttpResponse(f"Estás viendo la pregunta número {question_id}")
+
+
+def result(request, question_id):
+    return HttpResponse(f"Estás viendo los resultados de la pregunta número {question_id}")
+
+
+def vote(request, question_id):
+    return HttpResponse(f"Estás votando a la pregunta número {question_id}")
