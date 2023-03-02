810259ad63fbce18f6b569fa910f1c675bbd965d
Kevin Lopez G <kevin.lopez.gonzalez.it@gmail.com>
2023-03-01 22:02:49 -0300

    Se finalizan las vistas del curso mediante Functions Based Views

--- basic-django/premiosplatziapp/polls/views.py
+++ basic-django/premiosplatziapp/polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from django.http import HttpResponse
-from .models import Question
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from .models import Question, Choice
 
 # Create your views here.
 # Las Function Based Views siempre traen como parámetro un HttpRequest
@@ -19,8 +20,27 @@ def detail(request, question_id):
 
 
 def result(request, question_id):
-    return HttpResponse(f"Estás viendo los resultados de la pregunta número {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/result.html', {
+        'question': question
+    })
 
 
 def vote(request, question_id):
-    return HttpResponse(f"Estás votando a la pregunta número {question_id}")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "Seleccione una opción válida"
+        })
+    else:
+        choice.votes += 1
+        choice.save()
+        '''
+        HttpResponseRedirect a diferencia de HttpResponse, se asegura 
+        que el usuario no envíe una solicitud más de una vez por error.
+        '''
+        return HttpResponseRedirect(reverse('polls:result', args=(question.id,)))
+        
