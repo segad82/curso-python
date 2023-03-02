c0ffad0b8edceab64293646c4f45b76667a5c900
Kevin Lopez G <kevin.lopez.gonzalez.it@gmail.com>
2023-03-01 23:18:18 -0300

    Se transforma el uso de vistas mediante Generic Views

--- basic-django/premiosplatziapp/polls/urls.py
+++ basic-django/premiosplatziapp/polls/urls.py
@@ -5,11 +5,15 @@ app_name = 'polls'
 
 urlpatterns = [
     # ex: ~/polls/ => Estás en la página principal de Premios Platzi App
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
     # ex: ~/polls/2/ => Estás viendo la pregunta número 2
-    path('<int:question_id>/', views.detail, name='detail'),
+    #path('<int:question_id>/', views.detail, name='detail'),
     # ex: ~/polls/3/result => Estás viendo los resultados de la pregunta número 3
-    path('<int:question_id>/result', views.result, name='result'),
+    #path('<int:question_id>/result', views.result, name='result'),
     # ex: ~/polls/4/vote => Estás votando a la pregunta número 4
     path('<int:question_id>/vote', views.vote, name='vote'),
+
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/result', views.ResultView.as_view(), name='result'),
 ]
--- basic-django/premiosplatziapp/polls/views.py
+++ basic-django/premiosplatziapp/polls/views.py
@@ -1,30 +1,46 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
+from django.views import generic
 from .models import Question, Choice
 
 # Create your views here.
 # Las Function Based Views siempre traen como parámetro un HttpRequest
-def index(request):
-    latest_question_list = Question.objects.all()
-    return render(request, 'polls/index.html', {
-        'latest_question_list': latest_question_list
-    })
+#def index(request):
+#    latest_question_list = Question.objects.all()
+#    return render(request, 'polls/index.html', {
+#        'latest_question_list': latest_question_list
+#    })
 
 # En python es buena práctica dejar dos espacios en blanco entre funciones
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {
-        'question': question
-    })
+#def detail(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'polls/detail.html', {
+#        'question': question
+#    })
 
 
-def result(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/result.html', {
-        'question': question
-    })
+#def result(request, question_id):
+#    question = get_object_or_404(Question, pk=question_id)
+#    return render(request, 'polls/result.html', {
+#        'question': question
+#    })
 
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        # Retornar los últimos 5 elementos publicados
+        return Question.objects.order_by('-pub_date')[:5]
+    
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/result.html'
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
